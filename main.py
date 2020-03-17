54a6a98b15121b0fa655c08419b3b6af132965b4
ad101-lab <adickhans@gmail.com>
2020-03-17 14:41:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import smtplib
+
+name= open("names.txt", "w")
+email = open("emails.txt", "w")
+
+listToWrite = ["hi","this","is","a","test"]
+
+class email:
+  def openFiles():
+    #print("in progress")
+    name.
+    for 
+  def addEmail(email, name=""):
+    print("in progress")
+  def removeEmail(name):
+    removeNum = int(names.count(name))
+    names.remove()
