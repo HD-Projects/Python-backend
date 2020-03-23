bb379333ab78659cb44187ef7d1d5835c7df77dd
ad101-lab <adickhans@gmail.com>
2020-03-23 18:18:27 +0000

    caused errors

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ from lists import *
 import getpass
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
-context = ssl.create_default_context()
-s = smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context)
+import poplib
+from email import parser
 
 username = ""
 password = ""
-interactionCount = "0"
+interactionCounts = 0
 name = ""
 theirUsername = ""
 
@@ -20,7 +19,7 @@ listLen = 0
 login = 0 
 
 class email:
-  interactionCount = "0"
+  interactionCount = 0
   def DI(DebugLog):
     print("Debug Info: "+DebugLog)
     debugFile = open("debug.txt","a")
@@ -69,6 +68,12 @@ class email:
     names.remove(names[listLen])
     email.save()
   def sendEmail():
+    answer = str(input("This will send an email to all of these people \n"+str(names)+"\nAnd all these emails\n"+str(emails)+"\nWould you like to continue\nPress enter to continue and stop to return to main menu\n"))
+    if answer == "":
+      print("Continuing")
+    else:
+      email.DI("Failed to send email the user did not want to send to the list")
+      return 0;
     subject = input("What Is The Subject Of Your Message \n")
     sendingName = input("What do you want your name to show as\n")
     html = input("Do you want to send html content(Y/N) \n")
@@ -148,24 +153,52 @@ class email:
       s.sendmail(theirUsername, "debug.info.python.backend@gmail.com", text)
     except:
       print("Sending Debug Info Failed\nTry to login\n")
-  def choose():
-    interactionCount = 0
-    inputString = "What do you want to do:\n\nAdd An Email from you sending list(A)\nRemove an email from you sending list(R)\nSend an Email(S)\nLogin or change accounts(L)\nSend debug info (D)\n"
-    choice = input(inputString)
-    email.DI("Interaction #"+str(interactionCount)+" Choice of choice string "+choice)
-    if choice.lower() == "D":
-      email.DI("Interaction #"+str(interactionCount)+" Sending Debug Info!")
-      email.sendDebugInfo()
-      email.choose()
-    elif choice.lower() == "a":
-      email.DI("Interaction #"+str(interactionCount)+" Adding Email")
-      emailChoice = input("Do you want to upload a txt file with your contacts in it (a) or enter recivers manually(B)")
+  def showMailingList():
+    print("Emails in list:\n"+str(emails))
+    print("Names in list:\n"+str(names))
+    input("Press enter to continue...\n")
+  def checkMailbox():
+    try:
+      mail = imaplib.IMAP4_SSL(SMTP_SERVER)
+      mail.login(FROM_EMAIL,FROM_PWD)
+      mail.select('inbox')
+
+      type, data = mail.search(None, 'ALL')
+      mail_ids = data[0]
+
+      id_list = mail_ids.split()   
+      first_email_id = int(id_list[0])
+      latest_email_id = int(id_list[-1])
+
+
+      for i in range(latest_email_id,first_email_id, -1):
+        typ, data = mail.fetch(i, '(RFC822)' )
+
+        for response_part in data:
+          if isinstance(response_part, tuple):
+            msg = email.message_from_strin(response_part[1])
+          email_subject = msg['subject']
+          email_from = msg['from']
+          print ('From : ' + email_from + '\n')
+          print ('Subject : ' + email_subject +'\n')
+
+   def choose(interactionCounts):
+      inputString = "What do you want to do:\n\nAdd An Email from you sending list(A)\nRemove an email from you sending list(R)\nShow the mailing list(M)\nSend an Email(S)\nLogin or change accounts(L)\nSend debug info (D)\n Check your email (C)"
+      choice = input(inputString)
+      email.DI("Interaction #"+str(interactionCounts)+" Choice of choice string "+choice)
+      if choice.lower() == "D":
+        email.DI("Interaction #"+str(interactionCounts)+" Sending Debug Info!")
+        email.sendDebugInfo()
+        email.choose((interactionCounts+1))
+      elif choice.lower() == "a":
+        email.DI("Interaction #"+str(interactionCounts)+" Adding Email")
+        emailChoice = input("Do you want to upload a txt file with your contacts in it (a) or enter recivers manually(B)")
       if emailChoice.lower() == "b":
-        email.DI("Interaction #"+str(interactionCount)+" Manually Inputing emails")
+        email.DI("Interaction #"+str(interactionCounts)+" Manually Inputing emails")
         email.addEmail()
-        email.choose()
+        email.choose((interactionCounts+1))
       elif emailChoice.lower() == "a":
-        email.DI("Interaction #"+str(interactionCount)+" Adding File")
+        email.DI("Interaction #"+str(interactionCounts)+" Adding File")
         fileNameEmail = input("what is the name of the file of emails") 
         print("rename the file of emails to fileOfEmails.csv or fileOfEmails.txt depending on the formant")
         fileOfEmails = open(fileNameEmail, "r")
@@ -180,45 +213,56 @@ class email:
             r = email
             emails.append(email)
             emails.save()
-
+    
         elif fileOfEmails == "filesOfEmails.txt":
          for line in fileOfEmails:
           line = email
           emails.append(email)
           emails.save()
-
+    
         else:
           print("there was an error try again ")
-
+    
         if fileOfNames == "fileOfNames.txt":
           for line in fileOfNames:
             line = name
             names.append(name)
             names.save()
-
-
+    
+    
         elif fileOfNames == "fileOfNames.csv":
           reader = csv.reader(fileOfNames)
           next(reader) #skips header
           for r in reader:
             r = name
             names.append(name)
-            names.save(name)     
-        
+            names.save(name)
         else:
           print("there was an error, try again")
     elif choice.lower() == "r":
-      email.DI("Interaction #"+str(interactionCount)+" Remove Email")
+      email.DI("Interaction #"+str(interactionCounts)+" Remove Email")
       email.removeEmail()
-      email.choose()
+      email.choose((interactionCounts+1))
     elif choice.lower() == "s":
-      email.DI("Interaction #"+str(interactionCount)+" Sending Email")
+      email.DI("Interaction #"+str(interactionCounts)+" Sending Email")
       email.sendEmail()
-      email.choose()
+      email.choose((interactionCounts+1))
     elif choice.lower() == "l":
-      email.DI("Interaction #"+str(interactionCount)+" Reloging in")
+      email.DI("Interaction #"+str(interactionCounts)+" Reloging in")
       email.loginSMTP()
-      email.choose()
+      email.choose((interactionCounts+1))
+    elif choice.lower() == "m":
+      email.DI("Interaction #"+str(interactionCounts)+" showing mailing list")
+      email.showMailingList()
+      email.choose((interactionCounts+1))
+    elif choice.lower() == "d":
+      email.DI("Interaction #"+str(interactionCounts)+" send debug email")
+      email.sendDebugInfo()
+      email.choose((interactionCounts+1))
+    elif choice.lower() == "c":
+      email.DI("Interaction #"+str(interactionCounts)+" check mailbox")
+      email.checkMailbox()
+      email.choose((interactionCounts+1))
     else:
       email.DI("Session Finished, all personal information deleting...\n\n\n\n")
       email.sendDebugInfo()
@@ -227,6 +271,36 @@ class email:
       print("All personal information has been deleted")
 
 
+def search_string(uid_max, criteria):
+    c = list(map(lambda t: (t[0], '"'+str(t[1])+'"'), criteria.items())) + [('UID', '%d:*' % (uid_max+1))]
+    return '(%s)' % ' '.join(chain(*c))
+    # Produce search string in IMAP format:
+    #   e.g. (FROM "me@gmail.com" SUBJECT "abcde" BODY "123456789" UID 9999:*)
+
+
+def get_first_text_block(msg):
+    type = msg.get_content_maintype()
+
+    if type == 'multipart':
+        for part in msg.get_payload():
+            if part.get_content_maintype() == 'text':
+                return part.get_payload()
+    elif type == 'text':
+        return msg.get_payload()
+
+
+server = imaplib.IMAP4_SSL(imap_ssl_host, imap_ssl_port)
+server.login(username, password)
+server.select('INBOX')
+
+result, data = server.uid('search', None, search_string(uid_max, criteria))
+
+uids = [int(s) for s in data[0].split()]
+if uids:
+    uid_max = max(uids)
+    # Initialize `uid_max`. Any UID less than or equal to `uid_max` will be ignored subsequently.
+
+server.logout()
 
 try:
   if names[0] == names[0]:
@@ -235,8 +309,8 @@ try:
 except:
   names = ["Alex", "Riley","Alex"]
   emails = ["adickhans@gmail.com","rilesdk@gmail.com","dickha.alexan27@svvsd.org"]
-  email.save()
+  emails.save()
 
 interactionCount = 0
 email.loginSMTP()
-email.choose()
+email.choose((interactionCounts+1))
